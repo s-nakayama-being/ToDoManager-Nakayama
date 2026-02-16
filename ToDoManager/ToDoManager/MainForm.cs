2594c45be352b5d30c61a575a13d162d0c8fe0aa
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-02-16 14:19:33 +0900

    #601577【アプリ開発②】ToDoリスト選択時の詳細表示機能の追加
    メイン画面のTodoリストボックスでToDoのタイトルを選択した際、メイン画面右側の詳細エリアにその内容を即座に反映させる機能を実装しました。
    実装内容は以下の通りです。
    【MainForm.cs】
    ・詳細表示機能の実装
    　- リストボックスの選択変更イベント（SelectedIndexChanged）に対応するよう実装
    　- 選択アイテムのタイトル、内容、期限、状態を詳細表示エリアに反映
    　- 完了状態をチェックボックスのON/OFFで表現
    　- 期限切れタスク（未完了かつ今日より以前の日付）の場合、タイトル背景色を黄色に変更
    　- 未選択状態の場合、表示エリアをクリアする処理を追加
    ・リストボックスの空白部分をクリックした際、選択状態を解除する処理を追加
    ・詳細表示用コントロールを読み取り専用に設定する初期化処理を追加
    ・コンストラクタでの初期化順序を整理
    ・イベントハンドラの登録処理を追加

--- ToDoManager/ToDoManager/MainForm.cs
+++ ToDoManager/ToDoManager/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 using ToDoManager.Models;
 using ToDoManager.Services;
@@ -10,15 +11,27 @@ namespace ToDoManager {
     public partial class MainForm : Form {
         #region フィールド・初期化
         private readonly TodoService FService = new TodoService();
-
+              
         public MainForm() {
             InitializeComponent();
+
+            InitializeDetailArea();
 
             UpdateList();
         }
         #endregion
 
         #region UI操作
+        /// <summary>
+        /// 詳細表示エリアのコントロールを初期化
+        /// </summary>
+        private void InitializeDetailArea() {
+            FTxtTitle.ReadOnly = true;
+            FTxtContent.ReadOnly = true;
+            FDtpDueDate.Enabled = false;
+            FChkDone.Enabled = false;
+        }
+
         /// <summary>
         /// ToDoリストを更新
         /// </summary>
@@ -60,6 +73,34 @@ namespace ToDoManager {
                 }
             }
         }
+
+        /// <summary>
+        /// 選択されたアイテムの詳細を表示する
+        /// </summary>
+        /// <param name="vItem">表示対象のアイテム</param>
+        private void DisplaySelectedItemDetails(TodoItem vItem) {
+            FTxtTitle.Text = vItem.Title;
+            FTxtTitle.BackColor = SystemColors.Control;
+            FTxtContent.Text = vItem.Content;
+            FDtpDueDate.Text = vItem.DueDate.ToString("yyyy/M/d");
+
+            if (FChkDone is CheckBox wChk) wChk.Checked = vItem.IsCompleted;
+
+            if (!vItem.IsCompleted && vItem.DueDate<DateTime.Today) FTxtTitle.BackColor = Color.Yellow;
+        }
+
+        /// <summary>
+        /// 詳細表示エリアをクリアする
+        /// </summary>
+        private void ClearDetailDisplay() {
+            FTxtTitle.Text = string.Empty;
+            FTxtContent.Text = string.Empty;
+            FDtpDueDate.Text = string.Empty;
+
+            if (FChkDone is CheckBox wChk) wChk.Checked = false;
+
+            FTxtTitle.BackColor = SystemColors.Control;
+        }
         #endregion
 
         #region イベントハンドラ
@@ -75,6 +116,18 @@ namespace ToDoManager {
             } else {
                 MessageBox.Show(this, "指定ファイルが存在しません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+        private void FLstItems_SelectedIndexChanged(object sender, EventArgs e) {
+            if (FLstItems.SelectedItem is TodoItem wSelectedItem) {
+                DisplaySelectedItemDetails(wSelectedItem);
+            } else {
+                ClearDetailDisplay();
+            }
+        }
+        private void FLstItems_MouseDown(object sender, MouseEventArgs e) {
+            int wIndex = FLstItems.IndexFromPoint(e.Location);
+
+            if (wIndex == ListBox.NoMatches) FLstItems.SelectedIndex = -1;
         }
         #endregion
     }
