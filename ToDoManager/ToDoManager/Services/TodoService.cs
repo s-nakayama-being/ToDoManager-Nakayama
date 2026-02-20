0c2dc2b25172401a2f22b9f9066c17ef9d616eab
s-nakayama <s-nakayama@beingcorp.co.jp>
2026-02-20 13:11:23 +0900

    #602552【アプリ開発②】ToDoデータ読込時にファイルがロックされてしまう
    ファイルの読込を実行した後、続けてToDoの追加・更新を行おうとすると、ファイルが使用中となりI/Oエラーが発生するバグを修正しました。
    修正内容は以下の通りです。
    【TodoService.cs】
    ・Importメソッドにて、ファイルを読み込むStreamReaderのインスタンス生成部分にusingステートメントで囲み、読み込み完了後にファイルリソースが確実に解放されるよう修正。
    ・・ファイルパス（C_FilePath）にstatic readonlyを追加して定数を明示。

--- ToDoManager/ToDoManager/Services/TodoService.cs
+++ ToDoManager/ToDoManager/Services/TodoService.cs
@@ -13,7 +13,7 @@ namespace ToDoManager.Services {
         #region フィールド
         private List<TodoItem> FItems = new List<TodoItem>();
         private int FNextId = 1;
-        private string C_FilePath = "todos.xml";
+        private static readonly string C_FilePath = "todos.xml";
         #endregion
 
         /// <summary>
@@ -84,9 +84,7 @@ namespace ToDoManager.Services {
             if (!File.Exists(C_FilePath)) return false;
 
             var wSerializer = new XmlSerializer(typeof(List<TodoItem>));
-            var wStreamReader = new StreamReader(C_FilePath);
-            FItems = (List<TodoItem>)wSerializer.Deserialize(wStreamReader);
-            
+            using (var wStreamReader = new StreamReader(C_FilePath)) FItems = (List<TodoItem>)wSerializer.Deserialize(wStreamReader);
 
             FNextId = FItems.Any() ? FItems.Max(x => x.Id) + 1 : 1;
 
